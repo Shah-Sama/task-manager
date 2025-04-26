d3a265362476050dddd4e3a59f20301385acdd4f
shaheer <sarfat@students.cs.ubc.ca>
2025-04-26 19:37:24 +0300

    theme change

--- task-manager-backend/src/main/java/com/example/todo/service/TodoService.java
+++ task-manager-backend/src/main/java/com/example/todo/service/TodoService.java
@@ -1,47 +1,19 @@
 package com.example.todo.service;
 
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.example.todo.Todo;
 import com.example.todo.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TodoService {
-
     @Autowired
     private TodoRepository todoRepository;
 
-    @Autowired
-    private AmazonSQS amazonSQS;
-
-    @Autowired
-    private AmazonSNS amazonSNS;
-
-    @Value("${aws.sqs.todo-queue-url}")
-    private String todoQueueUrl;
-
-    @Value("${aws.sns.todo-topic-arn}")
-    private String todoTopicArn;
-
     public Todo createTodo(Todo todo) {
-        Todo savedTodo = todoRepository.save(todo);
-
-        // Send message to SQS for async processing
-        SendMessageRequest sendMessageRequest = new SendMessageRequest()
-                .withQueueUrl(todoQueueUrl)
-                .withMessageBody("New todo created: " + todo.getText());
-        amazonSQS.sendMessage(sendMessageRequest);
-
-        // Publish notification to SNS
-        amazonSNS.publish(todoTopicArn, "New todo created: " + todo.getText());
-
-        return savedTodo;
+        return todoRepository.save(todo);
     }
 
     public List<Todo> getAllTodos() {
@@ -49,16 +21,7 @@ public class TodoService {
     }
 
     public void deleteTodo(String id) {
-        Todo todo = todoRepository.findById(id);
-        if (todo != null) {
-            todoRepository.deleteById(id);
-
-            // Send message to SQS for async processing
-            SendMessageRequest sendMessageRequest = new SendMessageRequest()
-                    .withQueueUrl(todoQueueUrl)
-                    .withMessageBody("Todo deleted: " + todo.getText());
-            amazonSQS.sendMessage(sendMessageRequest);
-        }
+        todoRepository.deleteById(id);
     }
 
     public Todo updateTodo(String id, Todo todo) {
